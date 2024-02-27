0a1a26735499a240fe04a7bf3d0086c0c69267c1
theqly <theqlgames@gmail.com>
2024-02-27 14:18:57 +0700

    started lab3

---
+++ file_systems/lab3/main.c
@@ -0,0 +1,51 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+int main(int argc, char** argv){
+    if (argc != 2){
+        fprintf(stderr, "wrong number of args\n");
+        return 1;
+    }
+
+    pid_t pid;
+    char path[256];
+
+    if (strcmp(argv[1], "self") != 0) {
+        pid = atoi(argv[1]);
+        snprintf(path, sizeof(path), "/proc/%d/pagemap", pid);
+    } else {
+        snprintf(path, sizeof(path), "/proc/self/pagemap");
+        pid = getpid();
+    }
+
+    int fd = open(path, O_RDONLY);
+    if (fd < 0) {
+        perror("error with opening file");
+        return 1;
+    }
+
+    printf("Reading pagemap file for PID %d: %s\n", pid, path);
+
+    uint64_t entry;
+    size_t read_bytes;
+    uintptr_t virtual_address = 0;
+
+    while ((read_bytes = read(fd, &entry, sizeof(entry))) > 0) {
+        if (entry & (1ULL << 63)) {
+            uint64_t pfn = entry & PFN_MASK;
+            uintptr_t physical_address = pfn << 12;
+            printf("VA: 0x%016lx -> PA: 0x%016lx\n", virtual_address, physical_address);
+        }
+        virtual_address += sysconf(_SC_PAGE_SIZE);
+    }
+
+    if (read_bytes < 0) {
+        fprintf(stderr, "Error reading %s: %s\n", path, strerror(errno));
+        close(fd);
+        return 1;
+    }
+
+    close(fd);
+    return 0;
+}
