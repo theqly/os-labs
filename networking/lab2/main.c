ebe46b15a1e9ce23b22061bfa59a569ab1be6f25
theqly <theqlgames@gmail.com>
2024-03-19 01:21:43 +0700

    must be completed

--- networking/lab2/main.c
+++ networking/lab2/main.c
@@ -6,8 +6,8 @@
 #include <netdb.h>
 #include <unistd.h>
 
-static void udp_server(int port) {
-    const int server_fd = socket(PF_INET, SOCK_DGRAM, 0);
+static void tcp_server(int port) {
+    const int server_fd = socket(PF_INET, SOCK_STREAM, 0);
     if(server_fd == -1) {
         perror("socket error: ");
         return;
@@ -25,41 +25,59 @@ static void udp_server(int port) {
 
     printf("server is on port %d\n", (int) ntohs(server_addr.sin_port));
 
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-    int len;
+    if (listen(server_fd, 2) != 0) {
+        perror("listen error: ");
+        return;
+    }
+
+    struct sockaddr_storage client_addr;
+    socklen_t c_addr_len = sizeof(client_addr);
+    int client_fd, len;
+    pid_t pid;
 
     char message[1024];
     while (1) {
-        len = recvfrom(server_fd, message, sizeof(message), 0, (struct sockaddr*)&client_addr, &client_addr_len);
-        printf("client says: %s", message);
-        sendto(server_fd, message, len, 0, (struct sockaddr*)&client_addr, client_addr_len);
+        client_fd = accept(server_fd, (struct sockaddr*) &client_addr, &c_addr_len);
+        if(client_fd == -1) perror("accept error: ");
+        pid = fork();
+        if(pid == 0) {
+            close(server_fd);
+            while((len = recv(client_fd, message, sizeof(message), 0)) != 0) {
+                printf("client says: %s", message);
+                send(client_fd, message, len, 0);
+            }
+            close(client_fd);
+            return;
+        }
+
+        close(client_fd);
     }
 }
 
 static void client(int port) {
-    const int client_fd = socket(PF_INET, SOCK_DGRAM, 0);
+    const int client_fd = socket(PF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in client_addr = { 0 };
-    client_addr.sin_family = AF_INET;
-    client_addr.sin_port = htons(port);
+    struct sockaddr_in addr = { 0 };
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
 
     char addrstr[NI_MAXHOST + NI_MAXSERV + 1];
     snprintf(addrstr, sizeof(addrstr), "127.0.0.1:%d", port);
 
     inet_pton(AF_INET, addrstr, &addr.sin_addr);
 
-    struct sockaddr_in server_addr;
-    socklen_t client_addr_len = sizeof(server_addr);
-
+    if (connect(client_fd, (struct sockaddr*) &addr, sizeof(addr))) {
+        perror("connect error: ");
+        return;
+    }
     char message[1024];
     while (1) {
         fputs("enter your message: ", stdout);
         fgets(message,1024, stdin);
         if(!strcmp(message, "quit\n")) break;
-        sendto(client_fd, message, 1024, 0, &server_addr, );
+        send(client_fd, message, 1024, 0);
         int len = recv(client_fd, message, 1024, 0);
-        message[len-1] = '\0';
+        message[len] = '\0';
         printf("message from server: %s", message);
     }
 
@@ -74,7 +92,7 @@ int main(int argc, char *argv[]) {
     int port;
     sscanf(argv[2], "%d", &port);
     if(!strcmp(argv[1], "client"))  client(port);
-    else udp_server(port);
+    else tcp_server(port);
 
     return 0;
 }
