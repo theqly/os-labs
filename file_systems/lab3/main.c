e7a6fd32002d41dfbe2d7c002489b8589c09bab0
theqly <theqlgames@gmail.com>
2024-05-14 13:58:16 +0700

    AAAAAAAAAA

--- file_systems/lab3/main.c
+++ file_systems/lab3/main.c
@@ -1,58 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/types.h>
+#include <stdint.h>
+#include <string.h>
 
-#define PAGE_SIZE 4096
-#define PFN_MASK ((((uint64_t)1) << 55) - 1)
+#define PAGEMAP_ENTRY 8
+#define GET_PFN(X) X & 0x7FFFFFFFFFFFFF
 
-int main(int argc, char** argv){
-    if (argc != 2){
-        fprintf(stderr, "wrong number of args\n");
-        return 1;
+void read_pagemap(char * path_buf, unsigned long virt_addr){
+    FILE* f = fopen(path_buf, "rb");
+    if(!f){
+        perror("fopen error");
+        return;
     }
 
-    pid_t pid;
-    char path[256];
+    unsigned long file_offset = virt_addr / sysconf(_SC_PAGESIZE) * PAGEMAP_ENTRY;
 
-    if (strcmp(argv[1], "self") != 0) {
-        pid = atoi(argv[1]);
-        snprintf(path, sizeof(path), "/proc/%d/pagemap", pid);
-    } else {
-        snprintf(path, sizeof(path), "/proc/self/pagemap");
-        pid = getpid();
+    if(fseek(f, file_offset, SEEK_SET)){
+        perror("fseek error");
+        return;
     }
 
-    int fd = open(path, O_RDONLY);
-    if (fd < 0) {
-        perror("error with opening file");
-        return 1;
+    uint64_t physical_address = 0;
+    unsigned char char_buf[PAGEMAP_ENTRY];
+    int c;
+
+    for(int i = 0; i < PAGEMAP_ENTRY; ++i){
+        c = getc(f);
+        char_buf[PAGEMAP_ENTRY - i - 1] = c;
     }
 
-    uint64_t entry;
-    ssize_t read_bytes;
-    uintptr_t virtual_address = 0;
-
-    while ((read_bytes = read(fd, &entry, sizeof(entry))) > 0) {
-        if(entry & (1ULL << 63)){
-        uint64_t pfn = entry & PFN_MASK;
-        uintptr_t physical_address = pfn << 12;
-        printf("virtual: 0x%016lx -> physical: 0x%016lx\n", virtual_address, physical_address);
-        }
-
-        virtual_address += sysconf(_SC_PAGE_SIZE);
+    for(int i = 0; i < PAGEMAP_ENTRY; i++){
+        physical_address = (physical_address << 8) + char_buf[i];
     }
 
-    if (read_bytes < 0) {
-        fprintf(stderr, "Error reading %s: %s\n", path, strerror(errno));
-        close(fd);
-        return 1;
+    printf("Virtual address: 0x%lx -> Physical address: 0x%llx\n", virt_addr, (unsigned long long) GET_PFN(physical_address));
+
+    fclose(f);
+}
+
+int main(int argc, char ** argv){
+    if(argc != 3){
+        printf("Usage: ./lab3 <pid> <virtual address>\n");
+        return -1;
+    }
+    char path[100];
+
+    if(!memcmp(argv[1],"self",sizeof("self"))) sprintf(path, "/proc/self/pagemap");
+    else {
+        int pid = atoi(argv[1]);
+        sprintf(path, "/proc/%u/pagemap", pid);
     }
 
-    close(fd);
+    unsigned long virtual_address = strtol(argv[2], NULL, 16);
+
+    read_pagemap(path, virtual_address);
     return 0;
 }
