01232e19941d3e009d830f6c2da65a101b0e0b55
theqly <theqlgames@gmail.com>
2024-09-18 02:03:39 +0700

    lab4 done

--- threads/lab4/main.c
+++ threads/lab4/main.c
@@ -7,27 +7,37 @@
 
 void* mythread(void* arg) {
     int counter = 0;
+
+    char* str = malloc(20);
+    strcpy(str, "bebebe\n");
+
+    pthread_cleanup_push(free, str);
+
     while (1) {
         //printf("hello\n");
-        ++counter;
-        sleep(1);
+        //++counter;
+        printf("%s", str);
+        pthread_testcancel();
     }
-    pthread_exit(NULL);
+
+    pthread_cleanup_pop(1);
+
 }
 
 
 int main(void) {
-    printf("Hello, World!\n");
+    printf("start\n");
     pthread_t tid;
-    int err = pthread_create(&tid, NULL, mythread, NULL);
-    if (err) {
-        printf("main: pthread_create() failed: %s\n", strerror(err));
+
+    if (pthread_create(&tid, NULL, mythread, NULL)) {
+        fprintf(stderr, "main: pthread_create() failed\n");
         return -1;
     }
 
-    sleep(5);
+    sleep(1);
     pthread_cancel(tid);
 
+    pthread_join(tid, NULL);
 
     return 0;
 }
