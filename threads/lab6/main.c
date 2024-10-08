a56d2385611231e2d64f6e1e3747f53cbb26842a
theqly <theqlgames@gmail.com>
2024-10-08 19:57:21 +0700

    lab2.2 started

--- threads/lab6/main.c
+++ threads/lab6/main.c
@@ -1,4 +1,22 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
+#include <sched.h>
+
+void wrapper(int(*fn)(void* arg)) {
+    fn(NULL);
+
+}
+
+void my_thread_create(int(*fn)(void* arg)) {
+
+    //mmap stack
+    //get args
+
+    //clone(wrapper, )
+}
+
+
 
 int main(void)
 {
