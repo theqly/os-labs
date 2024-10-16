fcd2da9a9d7ac163676dc872aeccd4be5f7de2a9
theqly <theqlgames@gmail.com>
2024-10-16 23:50:19 +0700

    lab2 done

--- synchronization/lab2/queue.c
+++ synchronization/lab2/queue.c
@@ -1,119 +1,160 @@
 #define _GNU_SOURCE
-#include <pthread.h>
 #include <assert.h>
+#include <pthread.h>
 
 #include "queue.h"
 
 void *qmonitor(void *arg) {
-	queue_t *q = (queue_t *)arg;
+  queue_t *q = (queue_t *)arg;
 
-	printf("qmonitor: [%d %d %d]\n", getpid(), getppid(), gettid());
+  printf("qmonitor: [%d %d %d]\n", getpid(), getppid(), gettid());
 
-	while (1) {
-		queue_print_stats(q);
-		sleep(1);
-	}
+  while (1) {
+    queue_print_stats(q);
+    sleep(1);
+  }
 
-	return NULL;
+  return NULL;
 }
 
-queue_t* queue_init(int max_count) {
-	int err;
+queue_t *queue_init(int max_count) {
+  int err;
 
-	queue_t *q = malloc(sizeof(queue_t));
-	if (!q) {
-		printf("Cannot allocate memory for a queue\n");
-		abort();
-	}
+  queue_t *q = malloc(sizeof(queue_t));
+  if (!q) {
+    printf("Cannot allocate memory for a queue\n");
+    abort();
+  }
 
-	q->first = NULL;
-	q->last = NULL;
-	q->max_count = max_count;
-	q->count = 0;
+  q->first = NULL;
+  q->last = NULL;
+  q->max_count = max_count;
+  q->count = 0;
 
-	q->add_attempts = q->get_attempts = 0;
-	q->add_count = q->get_count = 0;
+  //pthread_spin_init(&q->lock, PTHREAD_PROCESS_PRIVATE);
+  //pthread_mutex_init(&q->mutex, PTHREAD_PROCESS_PRIVATE);
+  sem_init(&q->semaphore, 0, 3);
 
-	err = pthread_create(&q->qmonitor_tid, NULL, qmonitor, q);
-	if (err) {
-		printf("queue_init: pthread_create() failed: %s\n", strerror(err));
-		abort();
-	}
+  q->add_attempts = q->get_attempts = 0;
+  q->add_count = q->get_count = 0;
 
-	return q;
+  err = pthread_create(&q->qmonitor_tid, NULL, qmonitor, q);
+  if (err) {
+    printf("queue_init: pthread_create() failed: %s\n", strerror(err));
+    abort();
+  }
+
+  return q;
 }
 
 void queue_destroy(queue_t *q) {
 
-    qnode_t* current = q->first;
-    qnode_t* tmp;
+  qnode_t *current = q->first;
+  qnode_t *tmp;
 
-	for(int i = 0; i < q->count; ++i){
-        tmp = current->next;
-        free(current);
-        current = tmp;
-    }
+  for (int i = 0; i < q->count; ++i) {
+    tmp = current->next;
+    free(current);
+    current = tmp;
+  }
 
-    pthread_cancel(q->qmonitor_tid);
-    pthread_join(q->qmonitor_tid, NULL);
 
-    free(q);
+  //pthread_spin_destroy(&q->lock);
+  //pthread_mutex_destroy(&q->mutex);
+  sem_destroy(&q->semaphore);
+  pthread_cancel(q->qmonitor_tid);
+  pthread_join(q->qmonitor_tid, NULL);
+
+  free(q);
 }
 
 int queue_add(queue_t *q, int val) {
-	q->add_attempts++;
 
-	assert(q->count <= q->max_count);
+  //pthread_spin_lock(&q->lock);
+  //pthread_mutex_lock(&q->mutex);
+  sem_wait(&q->semaphore);
 
-	if (q->count == q->max_count)
-		return 0;
+  q->add_attempts++;
 
-	qnode_t *new = malloc(sizeof(qnode_t));
-	if (!new) {
-		printf("Cannot allocate memory for new node\n");
-		abort();
-	}
+  assert(q->count <= q->max_count);
 
-	new->val = val;
-	new->next = NULL;
+  if (q->count == q->max_count) {
+    //pthread_spin_unlock(&q->lock);
+    //pthread_mutex_unlock(&q->mutex);
+    sem_post(&q->semaphore);
+    return 0;
+  }
 
-	if (!q->first)
-		q->first = q->last = new;
-	else {
-		q->last->next = new;
-		q->last = q->last->next;
-	}
+  qnode_t *new = malloc(sizeof(qnode_t));
+  if (!new) {
+    printf("Cannot allocate memory for new node\n");
+    abort();
+  }
 
-	q->count++;
-	q->add_count++;
+  new->val = val;
+  new->next = NULL;
 
-	return 1;
+  if (!q->first)
+    q->first = q->last = new;
+  else {
+    q->last->next = new;
+    q->last = q->last->next;
+  }
+
+  q->count++;
+  q->add_count++;
+
+  //pthread_spin_unlock(&q->lock);
+  //pthread_mutex_unlock(&q->mutex);
+  sem_post(&q->semaphore);
+
+  return 1;
 }
 
 int queue_get(queue_t *q, int *val) {
-	q->get_attempts++;
 
-	assert(q->count >= 0);
+  //pthread_spin_lock(&q->lock);
+  //pthread_mutex_lock(&q->mutex);
+  sem_wait(&q->semaphore);
 
-	if (q->count == 0)
-		return 0;
+  q->get_attempts++;
 
-	qnode_t *tmp = q->first;
+  assert(q->count >= 0);
 
-	*val = tmp->val;
-	q->first = q->first->next;
+  if (q->count == 0) {
+    // pthread_spin_unlock(&q->lock);
+    // pthread_mutex_unlock(&q->mutex);
+    sem_post(&q->semaphore);
+    return 0;
+  }
 
-	free(tmp);
-	q->count--;
-	q->get_count++;
+  qnode_t *tmp = q->first;
 
-	return 1;
+  *val = tmp->val;
+  q->first = q->first->next;
+
+  free(tmp);
+  q->count--;
+  q->get_count++;
+
+  //pthread_spin_unlock(&q->lock);
+  //pthread_mutex_unlock(&q->mutex);
+  sem_post(&q->semaphore);
+
+  return 1;
 }
 
 void queue_print_stats(queue_t *q) {
-	printf("queue stats: current size %d; attempts: (%ld %ld %ld); counts (%ld %ld %ld)\n",
-		q->count,
-		q->add_attempts, q->get_attempts, q->add_attempts - q->get_attempts,
-		q->add_count, q->get_count, q->add_count -q->get_count);
-}
+  //pthread_spin_lock(&q->lock);
+  //pthread_mutex_lock(&q->mutex);
+  sem_wait(&q->semaphore);
 
+  printf("queue stats: current size %d; attempts: (%ld %ld %ld); counts (%ld "
+         "%ld %ld)\n",
+         q->count, q->add_attempts, q->get_attempts,
+         q->add_attempts - q->get_attempts, q->add_count, q->get_count,
+         q->add_count - q->get_count);
+  //pthread_spin_unlock(&q->lock);
+  //pthread_mutex_unlock(&q->mutex);
+  sem_post(&q->semaphore);
+}
