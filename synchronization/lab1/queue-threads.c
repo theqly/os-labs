c5bd59e0d1bacaa9f85e6e81e40a2aaf88f5dc08
theqly <theqlgames@gmail.com>
2024-12-03 20:10:24 +0700

    fix

--- synchronization/lab1/queue-threads.c
+++ synchronization/lab1/queue-threads.c
@@ -1,13 +1,12 @@
 #define _GNU_SOURCE
+#include <errno.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <string.h>
-#include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#include <pthread.h>
 #include <sched.h>
 
 #include "queue.h"
@@ -16,87 +15,101 @@
 #define NOCOLOR "\033[0m"
 
 void set_cpu(int n) {
-	int err;
-	cpu_set_t cpuset;
-	pthread_t tid = pthread_self();
+  int err;
+  cpu_set_t cpuset;
+  pthread_t tid = pthread_self();
 
-	CPU_ZERO(&cpuset);
-	CPU_SET(n, &cpuset);
+  CPU_ZERO(&cpuset);
+  CPU_SET(n, &cpuset);
 
-	err = pthread_setaffinity_np(tid, sizeof(cpu_set_t), &cpuset);
-	if (err) {
-		printf("set_cpu: pthread_setaffinity failed for cpu %d\n", n);
-		return;
-	}
+  err = pthread_setaffinity_np(tid, sizeof(cpu_set_t), &cpuset);
+  if (err) {
+    printf("set_cpu: pthread_setaffinity failed for cpu %d\n", n);
+    return;
+  }
 
-	printf("set_cpu: set cpu %d\n", n);
+  printf("set_cpu: set cpu %d\n", n);
 }
 
 void *reader(void *arg) {
-	int expected = 0;
-	queue_t *q = (queue_t *)arg;
-	printf("reader [%d %d %d]\n", getpid(), getppid(), gettid());
+  int expected = 0;
+  queue_t *q = (queue_t *)arg;
+  printf("reader [%d %d %d]\n", getpid(), getppid(), gettid());
 
-	set_cpu(1);
+  set_cpu(1);
 
-	while (1) {
-		int val = -1;
-		int ok = queue_get(q, &val);
-		if (!ok)
-			continue;
+  while (1) {
+    int val = -1;
+    int ok = queue_get(q, &val);
+    if (!ok)
+      continue;
 
-		if (expected != val)
-			printf(RED"ERROR: get value is %d but expected - %d" NOCOLOR "\n", val, expected);
+    if (expected != val)
+      printf(RED "ERROR: get value is %d but expected - %d" NOCOLOR "\n", val,
+             expected);
 
-		expected = val + 1;
-	}
+    expected = val + 1;
+  }
 
-	return NULL;
+  return NULL;
 }
 
 void *writer(void *arg) {
-	int i = 0;
-	queue_t *q = (queue_t *)arg;
-	printf("writer [%d %d %d]\n", getpid(), getppid(), gettid());
+  int i = 0;
+  queue_t *q = (queue_t *)arg;
+  printf("writer [%d %d %d]\n", getpid(), getppid(), gettid());
 
-	set_cpu(1);
+  set_cpu(1);
 
-	while (1) {
-		int ok = queue_add(q, i);
-		if (!ok)
-			continue;
-		i++;
-	}
+  while (1) {
+    int ok = queue_add(q, i);
+    if (!ok)
+      continue;
+    i++;
+    //usleep(1);
+  }
 
-	return NULL;
+  return NULL;
 }
 
 int main() {
-	pthread_t tid;
-	queue_t *q;
-	int err;
+  pthread_t reader_tid, writer_tid;
+  queue_t *q;
+  int err;
 
-	printf("main [%d %d %d]\n", getpid(), getppid(), gettid());
+  printf("main [%d %d %d]\n", getpid(), getppid(), gettid());
 
-	q = queue_init(100000);
+  q = queue_init(100000);
 
-	err = pthread_create(&tid, NULL, reader, q);
-	if (err) {
-		printf("main: pthread_create() failed: %s\n", strerror(err));
-		return -1;
-	}
+  err = pthread_create(&reader_tid, NULL, reader, q);
+  if (err) {
+    printf("main: pthread_create() failed: %s\n", strerror(err));
+    return -1;
+  }
 
-	//sched_yield();
+  sched_yield();
 
-	err = pthread_create(&tid, NULL, writer, q);
-	if (err) {
-		printf("main: pthread_create() failed: %s\n", strerror(err));
-		return -1;
-	}
+  err = pthread_create(&writer_tid, NULL, writer, q);
+  if (err) {
+    printf("main: pthread_create() failed: %s\n", strerror(err));
+    return -1;
+  }
 
-	// TODO: join threads
+  err = pthread_join(reader_tid, NULL);
+  if (err) {
+    printf("main: pthread_join() for reader failed: %s\n", strerror(err));
+    return -1;
+  }
 
-	pthread_exit(NULL);
+  err = pthread_join(writer_tid, NULL);
+  if (err) {
+    printf("main: pthread_join() for writer failed: %s\n", strerror(err));
+    return -1;
+  }
 
-	return 0;
+  queue_destroy(q);
+
+  pthread_exit(NULL);
+
+  return 0;
 }
